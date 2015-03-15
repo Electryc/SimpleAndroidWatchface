afa44f107f2551b5c1654ef15ed6a58a377e278e
Andrei Catinean <andrei@novoda.com>
2015-03-15 13:25:13 +0000

    Adds skeleton for mobile settings activity

---
+++ mobile/src/main/java/com/catinean/simpleandroidwatchface/SimpleWatchFaceConfigurationActivity.java
@@ -0,0 +1,13 @@
+package com.catinean.simpleandroidwatchface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleWatchFaceConfigurationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+}
